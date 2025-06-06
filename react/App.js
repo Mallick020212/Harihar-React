ef1bc39c762fbdfca56934254809954073a6fa85
Mallick020212 <86464685+Mallick020212@users.noreply.github.com>
2025-06-07 00:34:00 +0530

    chnages in index.html file

--- react/App.js
+++ react/App.js
@@ -29,7 +29,7 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child"},
+const parent=React.createElement("div",{id:"parent"},[React.createElement("div",{id:"child "},
 
     [React.createElement("h1",{id:"heading"},"hello h1 tag"),React.createElement("h2",{id:"heading"},"hello Harihar")]),
     React.createElement("div",{id:"child1"},
