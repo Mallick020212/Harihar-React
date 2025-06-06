c336fbd91a78cd5b260477e9c1dceddd9d561888
Mallick020212 <86464685+Mallick020212@users.noreply.github.com>
2025-06-06 14:42:53 +0530

    chnages

--- react/App.js
+++ react/App.js
@@ -34,4 +34,4 @@ const parent=React.createElement("div",{id:"parent"},[React.createElement("div",
     React.createElement("div",{id:"child1"},
         [React.createElement("h1",{id:"heading"},"hello h1 tag"),React.createElement("h2",{id:"heading"},"hello h2 tag")])]);
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);    
+root.render(parent);     
