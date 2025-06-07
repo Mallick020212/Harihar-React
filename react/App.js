1706e1ec93531dffdfd62ebe8454439b5bca5364
Mallick020212 <86464685+Mallick020212@users.noreply.github.com>
2025-06-08 01:23:10 +0530

    chnages in app js files

--- react/App.js
+++ react/App.js
@@ -32,17 +32,48 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child" }, [
-    React.createElement("h1", { key: "h1a",id:"heading" }, "hello h1 tag"),
-    React.createElement("h2", { key: "h2a",id:"heading" }, "hello Harihar")
-  ]),
-  React.createElement("div", { id: "child1", key: "child1" }, [
-    React.createElement("h1", { key: "h1b",id:"heading" }, "hello h1 tag"),
-    React.createElement("h2", { key: "h2b",id:"heading" }, "hello h2 tag")
-  ])
-]);
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child", key: "child" }, [
+//     React.createElement("h1", { key: "h1a",id:"heading" }, "hello h1 tag"),
+//     React.createElement("h2", { key: "h2a",id:"heading" }, "hello Harihar")
+//   ]),
+//   React.createElement("div", { id: "child1", key: "child1" }, [
+//     React.createElement("h1", { key: "h1b",id:"heading" }, "hello h1 tag"),
+//     React.createElement("h2", { key: "h2b",id:"heading" }, "hello h2 tag")
+//   ])
+// ]);
+
+/// React Elemet
+// const parent= ( <div>
+
+//   <h1 id="heading" className="h1tag"> Hellow react js this is Harihar Mallick</h1>
+//   <h1 id="heading" className="h1tag"> Hellow react js this is Harihar Mallick</h1>
+
+// </div>
+// );
+
+// functinal componet
+// functional componet is just javascript function which return the pices of jsx element or react element 
+// always start with componet name in capital letter 
+  // const Title=()=>(
+  //   <h1 id="heading">Hello this is title componet</h1>
+  // );
+  // const HeadingComponet=()=>(
+  //     <div>
+  //       <Title/>
+  //         <h2 className="heading" id="heading">Hello this is functional componet</h2>
+  //     </div>
+  // );
+
+
+   const title=( <h1 id="heading">Hello this is title componet</h1>);
+  const HeadingComponet=()=>(
+      <div>
+        {title}
+          <h2 className="heading" id="heading">Hello this is functional componet</h2>
+      </div>
+  );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponet/>);
  
